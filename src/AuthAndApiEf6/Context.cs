6e1dd8dc4eb798867978462ce21cb6146260ef63
Alexander Trauzzi <atrauzzi@gmail.com>
2016-05-25 19:29:33 +0000

    Let's see how this continues to grow...

---
+++ src/AuthAndApiEf6/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using AuthorizationRepository = AuthAndApi.Repository.Authorization<int>;
+
+
+namespace AuthAndApi.Ef6 {
+
+    public class Context : DbContext, AuthorizationRepository {
+
+        public DbSet<Authorization> Authorizations { get; set; }
+
+        public Authorization Get(int id) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Authorization> GetForOwner(Owner owner) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Authorization> UpdateOrCreate() {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ src/AuthAndApiEf6/readme.md
@@ -0,0 +1,11 @@
+﻿# Auth and API for Entity Framework 6
+
+This package has the classes necessary to use the Auth and API library with Entity Framework 6.
+
+# Meta
+
+## Resources
+
+ - [Getting Started with ASP.NET Core and Entity Framework 6](https://docs.asp.net/en/latest/data/entity-framework-6.html)
+ - [Repository Pattern with C# and Entity Framework, Done Right](https://www.youtube.com/watch?v=rtXpYpZdOzM)
+ - [The Clean Architecture](https://blog.8thlight.com/uncle-bob/2012/08/13/the-clean-architecture.html)
